3bdfabf76fdb9628e65286f2d49a467a6d776589
Oliver Sun <olisun@mac.com>
2020-02-07 13:41:00 -0800

    added content and images to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from "react-dom";
 import { WOW } from 'wowjs/dist/wow';
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Image, rounded, fluid } from "react-bootstrap";
 
 import Hero from "../components/Hero";
 
@@ -26,13 +26,30 @@ class HomePage extends Component {
         <div className="container">
           <Container>
             <Row>
-              <Col sm={8}>sm=8</Col>
-              <Col sm={4}>sm=4</Col>
+              <Col sm={8}>
+                <h5><strong>e360 microwave, inc is a diverse RF/Microwave technology provider founded on 30+ years of experience with innovation and engineering excellence.</strong></h5>
+              </Col>
+              <Col sm={4}>
+                <Image src="/images/logo.png" fluid />
+              </Col>
             </Row>
             <Row>
-              <Col sm>sm=true</Col>
-              <Col sm>sm=true</Col>
-              <Col sm>sm=true</Col>
+              <Col sm={8}>
+                <p>e360 microwave’s products and technology serve multiple applications within several primary end markets: wired infrastructure, wireless communications, enterprise, defense/government and others. Applications for our products in these markets include telecommunication infrastructure, base stations, point-to-point communications, broadband access and IoT. In addition, our products are used in all levels and phases of testing. Levels: semiconductor wafer probe testing, packaged devices, modules and the end product or system. Phases: bench testing, low rate production to highly sophisticated automatic test equipment (ATE) solutions for mass production.</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={8}>
+                <h5><strong>Our Value ​Proposition</strong></h5>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={8}>
+                <p>e360 microwave is a RF/Microwave technology focused company. We design and deliver high performance products with intrinsic reliability and low cost of ownership. We build long term partnerships with our employees and customers that are kept in perfect alignment through engagement, proactive communication and exceptional support</p>
+              </Col>
+              <Col sm>
+                <Image src="/images/logo.png" fluid />
+              </Col>
             </Row>
           </Container>
         </div>
