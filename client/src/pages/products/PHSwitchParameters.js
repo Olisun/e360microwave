68b56f89d2b20f5dfc9cfc25a15d095db1d1d89b
Oliver Sun <olisun@mac.com>
2020-02-17 18:04:44 -0800

    finished adding content to the power handling and swtch params product page

--- client/src/pages/products/PHSwitchParameters.js
+++ client/src/pages/products/PHSwitchParameters.js
@@ -21,10 +21,55 @@ class PHSwitchParameters extends Component {
             </Col>
           </Row>
         </Container>
-        <Container className="wow fadeInLeft">
-          <Col sm={6}>
-            <Image className="aboutImage-margin" src="/images/power-handling.png" fluid />
-          </Col>
+        <Container className="wow fadeIn">
+          <Row>
+            <Col sm={6}>
+              <Image className="aboutImage-margin" src="/images/power-handling.png" fluid />
+            </Col>
+            <Col sm={6}>
+              <h5 id="TypicalSwitchParams"><strong>Typical Switch Parameters</strong></h5>
+              <Table responsive bordered striped>
+                <tbody>
+                  <tr>
+                    <td>Switch Type:</td>
+                    <td>-Electromechanical, Coaxial</td>
+                  </tr>
+                  <tr>
+                    <td>RF Contracts:</td>
+                    <td>Break before Make</td>
+                  </tr>
+                  <tr>
+                    <td>Characteristic Impedance</td>
+                    <td>S0 Ohmn (75 Ohmn available for SPDT upon request)</td>
+                  </tr>
+                  <tr>
+                    <td>Temperature Range:</td>
+                    <td>-25° C to +65° C Operating</td>
+                  </tr>
+                  <tr>
+                    <td>Wider Temperature Range:</td>
+                    <td>-54° C to +85° C Operating</td>
+                  </tr>
+                  <tr>
+                    <td>Humidity:</td>
+                    <td>Moisture Seal Available</td>
+                  </tr>
+                  <tr>
+                    <td>Shock:</td>
+                    <td>MIL-STD-202-Method 213, Condition D (500G Non-Operating)</td>
+                  </tr>
+                  <tr>
+                    <td>Vibration:</td>
+                    <td>MIL-STD-202-Method 214, Condition D (10G RMS Non-Operating)</td>
+                  </tr>
+                  <tr>
+                    <td>Switches Life Actuations</td>
+                    <td>Failsafe - 2 Million Cycles; Latching - 5 Million Cycles; Normally Open - 2 Million Cycles</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
         </Container>
       </div>
     )
