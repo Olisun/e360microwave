e5c93d76a77b619f07fe8389f1897aba41b23ced
Oliver Sun <olisun@mac.com>
2020-02-27 11:39:47 -0800

    able to add a working example of a cascading dropdown to the cascading dropdown component

--- client/src/components/cascadingDropDown/CascadingDropDown.js
+++ client/src/components/cascadingDropDown/CascadingDropDown.js
@@ -1,59 +1,79 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import Select from "react-select";
-// import "react-select/dist/react-select.css";
 
 class CascadingDropDown extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      name: 'React',
-      selectedOption: {},
-      selectedOption2: {}
+      countries: [],
+      states: [],
+      cities: [],
+      selectedCountry: '--Choose Country--',
+      selectedState: '--Choose State--'
     };
+    this.changeCountry = this.changeCountry.bind(this);
+    this.changeState = this.changeState.bind(this);
   }
 
-  handleChange1 = (selectedOption) => {
-    this.setState({ selectedOption });
-  };
+  componentDidMount() {
+    this.setState({
+      countries: [
+        { name: 'Germany', states: [{ name: 'A', cities: ['Duesseldorf', 'Leinfelden-Echterdingen', 'Eschborn'] }] },
+        { name: 'Spain', states: [{ name: 'B', cities: ['Barcelona'] }] },
+        { name: 'USA', states: [{ name: 'C', cities: ['Downers Grove'] }] },
+        { name: 'Mexico', states: [{ name: 'D', cities: ['Puebla'] }] },
+        { name: 'India', states: [{ name: 'E', cities: ['Delhi', 'Kolkata', 'Mumbai', 'Bangalore'] }] },
+      ]
+    });
+  }
 
-  handleChange2 = (selectedOption) => {
-    this.setState({ selectedOption2: selectedOption })
+  changeCountry(event) {
+    this.setState({ selectedCountry: event.target.value });
+    this.setState({ states: this.state.countries.find(cntry => cntry.name === event.target.value).states });
+  }
+
+  changeState(event) {
+    this.setState({ selectedState: event.target.value });
+    const stats = this.state.countries.find(cntry => cntry.name === this.state.selectedCountry).states;
+    this.setState({ cities: stats.find(stat => stat.name === event.target.value).cities });
   }
 
   render() {
-    const options1 = [
-      { value: 'one', label: 'One' },
-      { value: 'two', label: 'Two' }
-    ];
-
-    const options2 = [
-      { value: 'one-a', label: 'One A', link: 'one' },
-      { value: 'one-b', label: 'One B', link: 'one' },
-      { value: 'two-a', label: 'Two A', link: 'two' },
-      { value: 'two-b', label: 'Two B', link: 'two' }
-    ];
-
-    const filteredOptions = options2.filter((o) => o.link === this.state.selectedOption.value)
-
     return (
-      <div>
-        <p>Select one first</p>
-        <Select
-          name="form-field-name"
-          value={this.state.selectedOption.value}
-          onChange={this.handleChange1}
-          options={options1}
-        />
-        <p>Then the other</p>
-        <Select
-          name="form-field-name"
-          value={this.state.selectedOption2.value}
-          onChange={this.handleChange2}
-          options={filteredOptions}
-        />
+      <div id="container">
+        <h2>Cascading or Dependent Dropdown using React</h2>
+
+        <div>
+          <label>Country</label>
+          <select placeholder="Country" value={this.state.selectedCountry} onChange={this.changeCountry}>
+            <option>--Choose Country--</option>
+            {this.state.countries.map((e, key) => {
+              return <option key={key}>{e.name}</option>;
+            })}
+          </select>
+        </div>
+
+        <div>
+          <label>State</label>
+          <select placeholder="State" value={this.state.selectedState} onChange={this.changeState}>
+            <option>--Choose State--</option>
+            {this.state.states.map((e, key) => {
+              return <option key={key}>{e.name}</option>;
+            })}
+          </select>
+        </div>
+
+        <div>
+          <label>City</label>
+          <select placeholder="City">
+            <option>--Choose City--</option>
+            {this.state.cities.map((e, key) => {
+              return <option key={key}>{e}</option>;
+            })}
+          </select>
+        </div>
       </div>
-    );
+    )
   }
 }
 
---
+++ client/src/components/cascadingDropDown/Test.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default ({ name }) => <h1>Test {name}!</h1>;
